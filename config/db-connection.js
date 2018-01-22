ac599e9f7236df92d6804f0e3cdf2c5e7bd9a66d
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2018-01-22 12:28:20 -0600

    clearDB settings

--- config/db-connection.js
+++ config/db-connection.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-var connection = mysql.createConnection({
+var db_config = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -12,6 +12,7 @@ var connection = mysql.createConnection({
     dateStrings: true 
 });
 
+/*
 connection.connect( err => {
     if (err) {
         console.log('Error trying to connect with Data Base: ' + err.stack);
@@ -19,6 +20,34 @@ connection.connect( err => {
     }
     console.log('Connected as id ' + connection.threadId);
 });
+*/
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }      
+        console.log('Connected as id ' + connection.threadId);                               	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+
 
 module.exports =  connection;
 
